75be3027e617063f321715fba46c64042224f24d
BMATHEW\BMATHEW <mathbles703>
2017-09-21 00:30:45 -0400

    Code review changes Removed unnecessary assignment Changed form to collective form

--- resources/views/navbar.blade.php
+++ resources/views/navbar.blade.php
@@ -13,7 +13,7 @@
                 <b>Siege Stats</b>
             </a>
 
-            @if($user = Auth::user())
+            @if(Auth::user())
                 <a href="#" class="navbar-brand dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">
                     User Statistics <span class="caret"></span>
                 </a>
@@ -28,15 +28,15 @@
         <div class="collapse navbar-collapse navbar-right" id="app-navbar-collapse">
             <ul class="nav navbar-nav">
                 @if($user = Auth::user())
-                    <form id="logout-form" role="form" method="POST" action="/logout">
-                        {{ csrf_field() }}
+                    {!! Form::open(['route' => 'logout', 'method' => 'post', 'id' => 'form-logout']) !!}
+                    {!! Form::token() !!}
                         <a href="#" class="navbar-brand dropdown-toggle" data-toggle="dropdown" role="button" ria-haspopup="true" aria-expanded="false">
                             <span class="glyphicon glyphicon-user aria-hidden="true""></span> {{ Auth::guard('web')->user()->email }} <span class="caret"></span>
                         </a>
                         <ul class="dropdown-menu">
                             <li><button type="submit">Logout</button></li>
                         </ul>
-                    </form>
+                    {!! Form::close() !!}
                 @else
                 <a href="{{ route('login') }}" class="btn btn-default navbar-btn">Login</a>
                 <a href="{{ route('register') }}" class="btn btn-default navbar-btn">Register</a>
