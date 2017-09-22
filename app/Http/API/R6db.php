bc9d1722b216a84f6cf79aab1d0417a996c20787
Matthew Pelletier <pelletier.j.matthew@gmail.com>
2017-09-22 03:38:59 -0400

    Comments -- because i'm a good boy

--- app/Http/API/R6db.php
+++ app/Http/API/R6db.php
@@ -2,10 +2,21 @@
 
 namespace App\Http\Api;
 
+/**
+ *  This class provides access to the R6db API using our unique x-app-id
+ *
+ *  NTOE: All GET requests must contain our x-app-id 
+ */
 class R6db {
     protected $URL = 'https://r6db.com/api/v2';
     protected $APP_ID = 'Boostin';
 
+    /**
+     * Searches for players by name
+     *
+     * @param   string  $name
+     * @return  json    Array of 'basic' player objects    
+     */
     public function getPlayers($name) {
         $curlHandle = curl_init($this->URL . '/players?name=' . $name);
         curl_setopt($curlHandle, CURLOPT_HTTPHEADER, ['accepts: application/json', 'x-app-id: ' . $this->APP_ID]);
@@ -13,6 +24,12 @@ class R6db {
         return curl_exec($curlHandle);
     }
 
+    /**
+     * Searches for a player by id
+     *
+     * @param   string  $id
+     * @return  json    'Detailed' player json object
+     */
     public function getPlayer($id) {
         $curlHandle = curl_init($this->URL . '/players/' . $id);
         curl_setopt($curlHandle, CURLOPT_HTTPHEADER, ['accepts: application/json', 'x-app-id: ' . $this->APP_ID]);
@@ -20,6 +37,9 @@ class R6db {
         return curl_exec($curlHandle);
     }
 
+    /**
+     *  Testing the route manually is getting me nothing
+     */
     public function getLeaderboard() {
 
     }
