75967d90e1945ba470f3c0fdef85be5574eeb91b
Matthew Pelletier <pelletier.j.matthew@gmail.com>
2017-09-26 10:46:42 -0400

    Hide large arrays in profile view

--- resources/views/player/profile.blade.php
+++ resources/views/player/profile.blade.php
@@ -9,6 +9,9 @@
     <pre>
         <?php
             $array = json_decode($player, true);
+            // Hiding large arrays
+            unset($array['progressions']); // stats over the last 30 days
+            unset($array['seasonRanks']); // seasonal stats Sherlock
             echo print_r($array, true);
         ?>
     </pre>
