a016f0ff8571f33673891a19e0eb6ebfae69d892
BMATHEW\BMATHEW <mathbles703>
2017-09-27 15:51:44 -0400

    Adjusted message

--- resources/views/player/player_home.blade.php
+++ resources/views/player/player_home.blade.php
@@ -6,7 +6,8 @@
     <div class="panel panel-default semi-transparent">
 
     @if(Auth::guard('web')->user()->user_id == null)
-        <h1>Oops, no email is linked to this account </h1>
+        <h1>Oops!</h1>
+        <h3>No R6 account is linked to this email</h3>
     @else
         <h1>Email: {{ Auth::guard('web')->user()->email }} </h1>
         <h1>UserId: {{ Auth::guard('web')->user()->user_id }} </h1>
