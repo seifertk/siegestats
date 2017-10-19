88e3dd0c2a653c1718b55f5c604f37a7cb7de466
Kyle Seifert <seifert.kyle@gmail.com>
2017-10-19 19:01:23 -0400

    add news & update page

--- routes/web.php
+++ routes/web.php
@@ -30,3 +30,4 @@ Route::get('/player/{id?}', 'PlayerController@show')->name('profile');
 Route::post('/search', 'PlayerController@search')->name('search');
 Route::post('/player/link', 'UserController@link')->name('link');
 Route::post('/player/compare', 'PlayerController@comparePlayers')->name('compare');
+Route::get('/news', 'NewsController@getNews')->name('news.index');
