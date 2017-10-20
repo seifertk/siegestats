3bc4897713c5d0587af75a36e1e3dcbf83971393
BMATHEW\BMATHEW <mathbles703>
2017-10-20 19:54:25 -0400

    Adjusted old formatting for operator ratios

--- resources/views/player/profile.blade.php
+++ resources/views/player/profile.blade.php
@@ -193,12 +193,12 @@
                                 </div>
 
                                 <div class="col-sm-2">
-                                    Win Ratio: {{number_format($operator->getWinLossRatio() * 100,2,'.','')}}%
+                                    Win Ratio: {{number_format($operator->getWinLossRatio(),2,'.','')}}
                                     
                                 </div>
 
                                 <div class="col-sm-2">
-                                    K/D Ratio: {{number_format($operator->getKillDeathRatio(),2,'.','')}}%
+                                    K/D Ratio: {{number_format($operator->getKillDeathRatio(),2,'.','')}}
                                 </div>
 
                                 <!--Time is in seconds. Convert to hours with seconds appended--> 
